43c6d217cfee366e61efe626f1fa1caa29e4066a
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-09-02 15:37:26 -0300

    CRUD que falta os toques finais

--- js/todo.js
+++ js/todo.js
@@ -4,29 +4,85 @@ var app = new Vue({
         message: 'Olá Vue',
         tasks: [
         ],
+        task: {
+            "id": null,
+            "title": null,
+            "dueTo": null,
+            "project": null,
+            "usuario": null,
+        },
+        taskFrom: {
+            "id": null,
+            "title": 'gggg',
+            "dueTo": null,
+            "project": null,
+            "usuario": null,
+        },
+        modoAdicionar: false,
     },
     methods: {
+        show() {
+            this.modoAdicionar = true
+        },
         getTasks() {
             fetch("http://localhost:3000/tasks")
                 .then((response) => response.json())
                 .then((tarefasJson) => (this.tasks = tarefasJson));
         },
         postTasks() {
-            console.log("post")
+            const data = this.task
+            const son = JSON.stringify(data)
+
+            fetch(`http://localhost:3000/tasks`, {
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                method: "POST",
+                body: son,
+            })
         },
-        editTask() {
-            console.log("edit")
+        editTaskPut() {
+            fetch(`http://localhost:3000/tasks/${this.taskFrom.id}`, {
+                method: "PUT",
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                body: JSON.stringify({
+                    title: this.taskFrom.title,
+                    project: this.taskFrom.project,
+                    dueTo: this.taskFrom.dueTo
+                })
+            })
+        },
+
+        editTask(tarefaId) {
+            //modal
+            console.log(tarefaId)
+            console.log(this.tasks)
+
+            let tarefa = this.tasks.filter((item) => item.id == tarefaId)[0]
+            console.log(tarefa)
+            this.taskFrom.id = tarefa.id
+            this.taskFrom.title = tarefa.title
+            this.taskFrom.project = tarefa.project
+            this.taskFrom.dueTo = tarefa.dueTo
+
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function () {
+                document.getElementById("modelWin").style.display = 'none'
+            }
+
+            // //edit tela
+            // this.taskFrom.id = tarefaId.id
+            // this.taskFrom.title = tarefaId.title
+
+            // //PUT API
+            // console.log("edit")
+
+
 
         },
-        deleteTask(tarefa) {
-            fetch(`http://localhost:3000/tasks/${tarefa.id}`, {
+        deleteTask(tarefaId) {
+            fetch(`http://localhost:3000/tasks/${tarefaId}`, {
                 method: "DELETE",
                 headers: { "Content-type": "application/json; charset=UTF-8" }
             })
-                .then((response) => response.json())
-                .then((tarefasJson) => (this.tasks = tarefasJson));
-            // this..delete(tarefa)
-            console.log("delete")
         }
     },
     created() {
--- js/todoNew.js
+++
@@ -1,6 +0,0 @@
-// var appNew = new Vue({
-//     el: "#appNew"
-//     data: {
-
-//     }
-// })
